d5d284e42303b7614ae1c1acd6690352c76610f7
Colin Robinson <colin.robinson@uib.no>
2024-02-05 11:31:59 +0100

    add image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+        container: {
+            center: true,
+        },
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
