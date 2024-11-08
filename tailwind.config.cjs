6ff472283482ed510b0bff5e361d75cb851f162c
Colin Robinson <colin.robinson@uib.no>
2024-11-08 15:26:08 +0100

    implement brand identity colors and dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         container: {
             center: true,
         },
+		extend: {
+			fontFamily: {
+				'sans': ['"Segoe Pro"', ...defaultTheme.fontFamily.sans],
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						'--tw-prose-body': '#002934',
+						'--tw-prose-headings': '#002934',
+						'--tw-prose-bold': '#002934',
+						maxWidth: "100ch"
+					}
+				}
+			}
+		}
+
 	},
+	darkMode: 'class',
 	plugins: [
 		require('@tailwindcss/typography'),
 	],
