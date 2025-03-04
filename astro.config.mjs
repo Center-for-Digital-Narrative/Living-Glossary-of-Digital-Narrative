822aff38c77edeb069eee186a1d8f75b41910b65
Colin Robinson <colin.robinson@uib.no>
2025-03-04 17:36:23 +0100

    ::see-also - use mdx and render with rehype from frontmatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import pagefind from "astro-pagefind";
 import mdx from "@astrojs/mdx";
-
+import rehypeSeeAlso from "./src/plugins/rehypeSeeAlso";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), pagefind(), mdx()],
-  prefetch: true,
+    markdown: {
+        rehypePlugins: [rehypeSeeAlso]
+    },
+  prefetch: true
 });
