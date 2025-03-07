6fc9f0f66b52aea3dd9beb527276f1b3424847c2
Colin Robinson <colin.robinson@uib.no>
2025-03-07 15:42:35 +0100

    dark mode for search. dark mode citation button. add CC license. Fix citation year

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,9 @@
-export function formatDate(date: Date|undefined): string {
+export function formatDate(date: Date|undefined, mode: string | null = null): string {
     if(!date) return '';
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-based; adding 1
     const day = date.getDate().toString().padStart(2, '0');
 
+    if(mode=='y') return `${year}`;
     return `${year}-${month}-${day}`;
 }
